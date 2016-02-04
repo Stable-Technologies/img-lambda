309a9f9d804241eed32fd57baecea3f27931402f
Nick <nick@stabtechs.com>
2016-02-04 12:44:52 -0800

    Some refactoring

---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+== Helpful Links ==
+http://www.jayway.com/2015/11/07/error-handling-in-api-gateway-and-aws-lambda/
--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 // dependencies
-var   gm      = require('gm').subClass({ imageMagick: true }) // Enable ImageMagick integration.
-    , async   = require('async')
-    , AWS     = require('aws-sdk');
+var gm      = require('gm').subClass({ imageMagick: true }) // Enable ImageMagick integration.
+  , async   = require('async')
+  , AWS     = require('aws-sdk');
     
 var s3 = new AWS.S3();
 
-var transform = function(masterKey, wantKey, srcBucket, imageType, context, width, height) {
+var validImageTypes = ['png', 'jpg', 'jpeg', 'gif'];
+
+var attemptConvert = function(originalKey, wantKey, imgReq, imageType, context) {
   async.waterfall([
     function download(next) {
       // Download the image from S3 into a buffer.
       s3.getObject({
-        Bucket : srcBucket,
-        Key    : masterKey
+        Bucket : imgReq.bucket,
+        Key    : originalKey
       }, next);
     },
-    function tranform(response, next) {
+    function convert(response, next) {
       gm(response.Body).size(function(err, size) {
         // Transform the image buffer in memory.
-        this.resize(width, height)
+        this.resize(imgReq.width, imgReq.height)
           .toBuffer(imageType, function(err, buffer) {
             if (err) {
               next(err);
@@ -30,69 +32,55 @@ var transform = function(masterKey, wantKey, srcBucket, imageType, context, widt
     function upload(contentType, data, next) {
       // Stream the transformed image to a different S3 bucket.
       s3.putObject({
-        Bucket      : srcBucket,
+        Bucket      : imgReq.bucket,
         Key         : wantKey,
         ACL         : 'public-read',
         Body        : data,
         ContentType : contentType
       }, next);
     },
-    function wat(response,next) { 
-      context.fail(s3.endpoint.href+srcBucket+"/"+wantKey);
+    function success(response,next) {
+      console.log("???????????")
+      console.log(response)
+      context.fail(s3.endpoint.href+imgReq.bucket+"/"+wantKey);
     }
   ],
   function (err) { 
-    console.log("Unable to resize!",err);
     context.fail("NotFound");
   });
 };
 
 exports.handler = function(imgReq, context) {
-    var srcBucket = "roceteer-dev-app"
-    var srcKey = imgReq.source;
+  var typeMatch = imgReq.source.match(/\.([^.]*)$/);
+  if(!typeMatch) {
+    console.log('Invalid Image Type Requested')
+    context.fail("BadRequest");
+    return;
+  }
 
-    var typeMatch = srcKey.match(/\.([^.]*)$/);
+  var imageType = typeMatch[1];
+  if (validImageTypes.indexOf(imageType.toLowerCase()) < 0) {
+    console.log('Invalid Image type requested ' + imgReq.source);
+    context.fail("BadRequest");
+    return;
+  }
 
-    if(!typeMatch) {
-      context.fail("BadRequest");
-      return;
+  var sourceRoot = imgReq.source.slice(0,-(imageType.length+1));
+  var wantKey = sourceRoot + "/" + imgReq.width + "_" + imgReq.height + "." + imageType;
+  async.waterfall([
+    function check(next) {
+      // Download the image from S3 into a buffer.
+      s3.headObject({
+        Bucket : imgReq.bucket,
+        Key    : wantKey
+      }, next);
+    },
+    function exists(response,next) {
+      context.fail(s3.endpoint.href+imgReq.bucket+"/"+wantKey);
     }
-
-    var validImageTypes = ['png', 'jpg', 'jpeg', 'gif'];
-
-    var imageType = typeMatch[1];
-    if (validImageTypes.indexOf(imageType.toLowerCase()) < 0) {
-      console.log('Invalid Image type requested ' + srcKey);
-      context.fail("BadRequest");
-      return;
-    }
-
-    var sourceBase = srcKey.slice(0,-4);
-
-    var wantKey = sourceBase + "/" + imgReq.width + "_" + imgReq.height + "." + imageType;
-
-    console.log("WANT TO LOAD: " + wantKey);
-
-    async.waterfall([
-        function check(next) {
-	    // Download the image from S3 into a buffer.
-	    s3.headObject({
-              Bucket : srcBucket,
-	      Key    : wantKey
-            }, next);
-        },
-        function wat(response,next) {
-          context.fail(s3.endpoint.href+srcBucket+"/"+wantKey);
-        }
-    ],
-    function(err) {
-      var originalKey = sourceBase + "/" + "original";
-      transform(originalKey,wantKey,srcBucket,imageType,context,imgReq.width,imgReq.height);
-    });    
-    //if(imgReq.height === 42) {
-    //    context.fail("Bad")
-    //} else {
-    //    context.fail("NotFound")
-    //}
-    //context.succeed({location : "http://example.com"})
+  ],
+  function(err) {
+    var originalKey = sourceRoot + "/" + "original";
+    attemptConvert(originalKey,wantKey,imgReq,imageType,context);
+  });
 };
